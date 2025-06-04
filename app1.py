e2828a094e47da87f1b23621ce7037044d18d590
widmer07 <47278837+widmer07@users.noreply.github.com>
2025-06-04 17:42:00 -0500

    Update app1.py

--- app1.py
+++ app1.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/hola")
 def hola():
-    return Response("HOLA DESDE API PYTHON", mimetype='text/plain')
+    return Response("HOLA DESDE API FASK PYTHON ", mimetype='text/plain')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
